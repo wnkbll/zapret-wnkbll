57d1908865a2bc1f96254f2e0a936130a3d0f878
Grigoriy Arzhevitin <arzhevitin.g@gmail.com>
2025-04-22 18:36:00 +0500

    fix scripts

--- zapret-winws/service_create.cmd
+++ zapret-winws/service_create.cmd
@@ -1,11 +1,9 @@
 set ARGS=^
 --wf-tcp=80,443 --wf-udp=443,50000-50100 ^
---filter-tcp=80 --dpi-desync=fakedsplit --dpi-desync-autottl=2 --dpi-desync-fooling=md5sig --new ^
---filter-tcp=443 --hostlist=\"%~dp0files\list-youtube.txt\" --dpi-desync=multidisorder --dpi-desync-split-pos=1,midsld --dpi-desync-repeats=11 --dpi-desync-fooling=md5sig --dpi-desync-fake-tls-mod=rnd,dupsid,sni=www.google.com --new ^
---filter-tcp=443 --dpi-desync=multidisorder --dpi-desync-split-pos=midsld --dpi-desync-repeats=6 --dpi-desync-fooling=badseq,md5sig --new ^
---filter-udp=443 --hostlist=\"%~dp0files\list-youtube.txt\" --dpi-desync=fakedsplit --dpi-desync-repeats=11 --dpi-desync-fake-quic=\"%~dp0files\quic_initial_www_google_com.bin\" --new ^
---filter-udp=443 --dpi-desync=multidisorder --dpi-desync-repeats=11 --new ^
---filter-udp=50000-50099 --filter-l7=discord,stun --dpi-desync=fake
+--filter-udp=443 --hostlist=\"%~dp0files\list-general.txt\" --dpi-desync=fake --dpi-desync-repeats=6 --dpi-desync-fake-quic=\"%~dp0files\quic_initial_www_google_com.bin\" --new ^
+--filter-udp=50000-50100 --filter-l7=discord,stun --dpi-desync=fake --dpi-desync-repeats=6 --new ^
+--filter-tcp=80 --hostlist=\"%~dp0files\list-general.txt\" --dpi-desync=fake,split2 --dpi-desync-autottl=2 --dpi-desync-fooling=md5sig --new ^
+--filter-tcp=443 --hostlist=\"%~dp0files\list-general.txt\" --dpi-desync=split --dpi-desync-split-pos=1 --dpi-desync-autottl --dpi-desync-fooling=badseq --dpi-desync-repeats=8
 
 call :srvinst winws1
 goto :eof
