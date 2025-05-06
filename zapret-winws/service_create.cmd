f9cb1d155166c22d1eb0917422443725f936e098
Grigoriy Arzhevitin <arzhevitin.g@gmail.com>
2025-05-06 19:38:38 +0500

    version bump

--- zapret-winws/service_create.cmd
+++ zapret-winws/service_create.cmd
@@ -1,9 +1,14 @@
+set "FILES=%~dp0files\"
+
 set ARGS=^
 --wf-tcp=80,443 --wf-udp=443,50000-50100 ^
---filter-udp=443 --hostlist=\"%~dp0files\list-general.txt\" --dpi-desync=fake --dpi-desync-repeats=6 --dpi-desync-fake-quic=\"%~dp0files\quic_initial_www_google_com.bin\" --new ^
+--filter-udp=443 --hostlist="%FILES%list-general.txt" --dpi-desync=fake --dpi-desync-repeats=6 --dpi-desync-fake-quic="%FILES%quic_initial_www_google_com.bin" --new ^
 --filter-udp=50000-50100 --filter-l7=discord,stun --dpi-desync=fake --dpi-desync-repeats=6 --new ^
---filter-tcp=80 --hostlist=\"%~dp0files\list-general.txt\" --dpi-desync=fake,split2 --dpi-desync-autottl=2 --dpi-desync-fooling=md5sig --new ^
---filter-tcp=443 --hostlist=\"%~dp0files\list-general.txt\" --dpi-desync=split --dpi-desync-split-pos=1 --dpi-desync-autottl --dpi-desync-fooling=badseq --dpi-desync-repeats=8
+--filter-tcp=80 --hostlist="%FILES%list-general.txt" --dpi-desync=fake,split2 --dpi-desync-autottl=2 --dpi-desync-fooling=md5sig --new ^
+--filter-tcp=443 --hostlist="%FILES%list-general.txt" --dpi-desync=fake,multidisorder --dpi-desync-split-pos=midsld --dpi-desync-repeats=8 --dpi-desync-fooling=md5sig,badseq --new ^
+--filter-udp=443 --ipset="%FILES%ipset-cloudflare.txt" --dpi-desync=fake --dpi-desync-repeats=6 --dpi-desync-fake-quic="%FILES%quic_initial_www_google_com.bin" --new ^
+--filter-tcp=80 --ipset="%FILES%ipset-cloudflare.txt" --dpi-desync=fake,split2 --dpi-desync-autottl=2 --dpi-desync-fooling=md5sig --new ^
+--filter-tcp=443 --ipset="%FILES%ipset-cloudflare.txt" --dpi-desync=fake,multidisorder --dpi-desync-split-pos=midsld --dpi-desync-repeats=6 --dpi-desync-fooling=md5sig,badseq
 
 call :srvinst winws1
 goto :eof
